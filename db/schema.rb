40e6104b687e96b2b10e9f5f64f19a31093e743f
Sean Brewer <seabre986@gmail.com>
2012-09-15 18:38:29 -0400

    Add name field to expenditure model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120915221437) do
+ActiveRecord::Schema.define(:version => 20120915223754) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(:version => 20120915221437) do
     t.integer  "instance_id"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.string   "name"
   end
 
   add_index "expenditures", ["category_id"], :name => "index_expenditures_on_category_id"
